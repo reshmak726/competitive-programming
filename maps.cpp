9363ba83d9f7557fed59c5d95dc26ade0900e20b
reshmak726 <reshmak726@eng.rizvi.edu>
2021-09-23 19:50:00 +0530

    multimap

--- maps.cpp
+++ maps.cpp
@@ -20,16 +20,23 @@ int main(){
     // } 
 
     // sum on map
-    map<string,int> m;
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        string s;
-        cin>>s;
-        // m[s] = m[s]+1;
-        m[s]++;
-    }
-    for(auto pr:m){
-        cout<<pr.first<<" "<<pr.second<<endl;
-    }
-}
+    // map<string,int> m;
+    // int n;
+    // cin>>n;
+    // for(int i=0;i<n;i++){
+    //     string s;
+    //     cin>>s;
+    //     // m[s] = m[s]+1;
+    //     m[s]++;
+//     }
+//     for(auto pr:m){
+//         cout<<pr.first<<" "<<pr.second<<endl;
+//     }
+
+
+
+// unordered maps are same but the keys are not in sorted order in final output time complexity becomes O(1) 
+
+// multimap is used to store multiple values
+// }
+
