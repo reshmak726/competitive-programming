2a9d187234eab79caaf62d3c01b95134135b0df6
reshmak726 <reshmak726@eng.rizvi.edu>
2021-09-30 12:51:12 +0530

    Nth root code

---
+++ nthroot.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+double eps = 1e-6;
+double multiply(double mid,int n){
+    double ans=1;
+    for(int i=0;i<n;i++){
+        ans*=mid;
+    }
+    return ans;
+}
+int main(){
+    double x;
+    int n;
+    cin>>x>>n;
+    double lo=1,hi=x,mid;
+    while(hi-lo>eps){
+        mid=(hi+lo)/2;
+        if(multiply(mid,n) <x){
+            lo=mid;
+        }else{
+            hi=mid;
+        }
+    }
+    cout<<lo<<endl;
+
+}
